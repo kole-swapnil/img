4cbd147a2f27edc3c202ba87f0b4c22e1fa47adf
Swapnil Kole <swapnilkole7500@gmail.com>
2020-12-16 00:49:30 +0530

    jhv

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const multer = require("multer");
 const path = require("path");
 
-  
+
 const storage = multer.diskStorage({
     destination : './upload/images',
     filename : (req ,file,cb) => {
-        return cb(null,`image_${Date.now()}${(path.extname(file.originalname)).toLowerCase()}`)
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 
@@ -18,14 +18,10 @@ const upload = multer({
 app.use('/profile',express.static('upload/images'));
 
 app.post("/upload",upload.single('profile'),(req,res) => {
-    console.log("req",req.file)
-    uploadFile(req.file, function(url){
-        res.json({
-            success : 1,
-            profile_url: url
-        })    
-    });
-    
+    res.json({
+        success : 1,
+        profile_url: `http://localhost:4000/profile/${req.file.filename}`
+    })
 })
 
 app.listen(4000, () => {
